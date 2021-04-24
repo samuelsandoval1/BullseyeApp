5b1d69bf7544e1aabe9d44e179c4b919b1cca875
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-04-24 10:41:33 -0700

    Initial Commit

---
+++ BullseyeApp/BullseyeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeAppApp.swift
+//  BullseyeApp
+//
+//  Created by Samuel Sandoval on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
